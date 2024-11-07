fed11b828ac7f7fab1c16d78c46e61e325dc5c03
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2024-11-07 21:52:23 +0700

    update homepage

--- Swap-n-Share/FrmHomePage.Designer.cs
+++ Swap-n-Share/FrmHomePage.Designer.cs
@@ -28,15 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHomePage));
             panel1 = new System.Windows.Forms.Panel();
-            panel2 = new System.Windows.Forms.Panel();
-            panel3 = new System.Windows.Forms.Panel();
+            button7 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
+            panel2 = new System.Windows.Forms.Panel();
+            button5 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            PanelSlide = new System.Windows.Forms.Panel();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            pictureBox3 = new System.Windows.Forms.PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
             panel1.Controls.Add(label1);
             panel1.Dock = System.Windows.Forms.DockStyle.Top;
             panel1.Location = new System.Drawing.Point(0, 0);
@@ -44,52 +65,220 @@
             panel1.Size = new System.Drawing.Size(2054, 69);
             panel1.TabIndex = 0;
             // 
-            // panel2
+            // button7
             // 
-            panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            panel2.Location = new System.Drawing.Point(0, 69);
-            panel2.Name = "panel2";
-            panel2.Size = new System.Drawing.Size(95, 1012);
-            panel2.TabIndex = 1;
+            button7.BackColor = System.Drawing.Color.Transparent;
+            button7.Image = (System.Drawing.Image)resources.GetObject("button7.Image");
+            button7.Location = new System.Drawing.Point(1893, -3);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(82, 72);
+            button7.TabIndex = 4;
+            button7.UseVisualStyleBackColor = false;
             // 
-            // panel3
+            // button6
             // 
-            panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            panel3.Location = new System.Drawing.Point(95, 69);
-            panel3.Name = "panel3";
-            panel3.Size = new System.Drawing.Size(400, 1012);
-            panel3.TabIndex = 2;
+            button6.BackColor = System.Drawing.Color.Transparent;
+            button6.Image = (System.Drawing.Image)resources.GetObject("button6.Image");
+            button6.Location = new System.Drawing.Point(1972, -3);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(82, 72);
+            button6.TabIndex = 2;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label1.ForeColor = System.Drawing.Color.FromArgb(17, 64, 187);
             label1.Location = new System.Drawing.Point(16, 14);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(232, 39);
             label1.TabIndex = 3;
             label1.Text = "Swap n Share";
             // 
+            // panel2
+            // 
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            panel2.Location = new System.Drawing.Point(0, 69);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(95, 1012);
+            panel2.TabIndex = 1;
+            // 
+            // button5
+            // 
+            button5.BackColor = System.Drawing.Color.Transparent;
+            button5.Image = (System.Drawing.Image)resources.GetObject("button5.Image");
+            button5.Location = new System.Drawing.Point(0, 852);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(95, 81);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = System.Drawing.Color.Transparent;
+            button4.Image = (System.Drawing.Image)resources.GetObject("button4.Image");
+            button4.Location = new System.Drawing.Point(0, 76);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(95, 81);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = System.Drawing.Color.Transparent;
+            button2.Image = (System.Drawing.Image)resources.GetObject("button2.Image");
+            button2.Location = new System.Drawing.Point(-1, 931);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(95, 81);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.Transparent;
+            button1.Image = (System.Drawing.Image)resources.GetObject("button1.Image");
+            button1.Location = new System.Drawing.Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(95, 81);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // PanelSlide
+            // 
+            PanelSlide.Dock = System.Windows.Forms.DockStyle.Left;
+            PanelSlide.Location = new System.Drawing.Point(95, 69);
+            PanelSlide.Name = "PanelSlide";
+            PanelSlide.Size = new System.Drawing.Size(400, 1012);
+            PanelSlide.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            pictureBox1.Image = Properties.Resources.SnS_LogoB;
+            pictureBox1.Location = new System.Drawing.Point(1162, 172);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(326, 346);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Century Gothic", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label2.ForeColor = System.Drawing.Color.FromArgb(17, 64, 187);
+            label2.Location = new System.Drawing.Point(1107, 503);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(274, 63);
+            label2.TabIndex = 4;
+            label2.Text = "Welcome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Century Gothic", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label3.ForeColor = System.Drawing.Color.FromArgb(255, 146, 13);
+            label3.Location = new System.Drawing.Point(1371, 503);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(173, 63);
+            label3.TabIndex = 5;
+            label3.Text = "Back!";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label4.ForeColor = System.Drawing.Color.Black;
+            label4.Location = new System.Drawing.Point(640, 590);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(1307, 78);
+            label4.TabIndex = 5;
+            label4.Text = "Join a thriving community where you can easily barter or rent items, \r\nconnect with trusted neighbors, and save money while sharing resources together.";
+            label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            pictureBox2.Image = (System.Drawing.Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new System.Drawing.Point(1240, 693);
+            pictureBox2.Margin = new System.Windows.Forms.Padding(5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(80, 80);
+            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            pictureBox3.Image = (System.Drawing.Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new System.Drawing.Point(1320, 673);
+            pictureBox3.Margin = new System.Windows.Forms.Padding(5);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new System.Drawing.Size(131, 115);
+            pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 30;
+            timer1.Tick += timer1_Tick;
+            // 
             // FrmHomePage
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.White;
             ClientSize = new System.Drawing.Size(2054, 1081);
-            Controls.Add(panel3);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(PanelSlide);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "FrmHomePage";
             Text = "FrmHomePage";
+            Load += FrmHomePage_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel PanelSlide;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
