0d28c0bc29044da3b211a7e0b4bd845f28bfb5d1
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2024-11-07 20:51:28 +0700

    new home page

---
+++ Swap-n-Share/FrmHomePage.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Swap_n_Share
+{
+    partial class FrmHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new System.Windows.Forms.Panel();
+            panel2 = new System.Windows.Forms.Panel();
+            panel3 = new System.Windows.Forms.Panel();
+            label1 = new System.Windows.Forms.Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(2054, 69);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            panel2.Location = new System.Drawing.Point(0, 69);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(95, 1012);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            panel3.Location = new System.Drawing.Point(95, 69);
+            panel3.Name = "panel3";
+            panel3.Size = new System.Drawing.Size(400, 1012);
+            panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label1.Location = new System.Drawing.Point(16, 14);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(232, 39);
+            label1.TabIndex = 3;
+            label1.Text = "Swap n Share";
+            // 
+            // FrmHomePage
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(2054, 1081);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "FrmHomePage";
+            Text = "FrmHomePage";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
